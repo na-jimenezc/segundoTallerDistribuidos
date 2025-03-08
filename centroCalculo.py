2768b279aca75b201620b8eabb63b5f2502d94a3
na-jimenezc <141644016+na-jimenezc@users.noreply.github.com>
2025-03-08 12:36:05 -0500

    SUBIDA INICIAL
    Hace falta el operador1, operador2 y manejo de errores en el centro de calculo y en el cliente. Se podría manejar con timeouts

---
+++ centroCalculo.py
@@ -0,0 +1,52 @@
+    import grpc
+    from concurrent import futures
+    import sumadorVector_pb2
+    import sumadorVector_pb2_grpc
+
+    class CentroCalculo(sumadorVector_pb2_grpc.CentroCalculoServicer):
+        def __init__(self):
+            #Se da la conexión al operador 1 en 50051
+            self.canalOP1 = grpc.insecure_channel('localhost:50051')
+            self.stubOP1 = sumadorVector_pb2_grpc.Operador1Stub(self.canalOP1)
+            
+            #Se da la conexión al operador 2 en 50052
+            self.canalOP2 = grpc.insecure_channel('localhost:50052')
+            self.stubOP2 = sumadorVector_pb2_grpc.Operador2Stub(self.canalOP2)
+
+        def SumarVector(self, request, context):
+            try:
+                '''Se divide al vector recibido en 2 partes'''
+                numeros = request.numeros
+                mitad = len(numeros)//2
+                primeraParte = numeros[:mitad]
+                segundaParte = numeros[mitad:]
+
+                '''Se envia la primera parte al operador 1 y se espera al response para enviar a la otra parte'''
+                responseOP1 = self.stubOP1.SumarParcial(sumadorVector_pb2.VectorRequest(numeros=primeraParte))
+
+                '''Se envia segunda parte al operador 2 y se espera al response final'''
+                responseOP2 = self.stubOP2.SumarFinal(sumadorVector_pb2.SumaParcialRequest(
+                    sumaParcial=responseOP1.sumaParcial,
+                    numeros=segundaParte
+                ))
+
+                '''Una vez se tiene el response final, se devuelve al cliente. De lo contrario sale el error'''
+                return sumadorVector_pb2.SumaResponse(resultado=responseOP2.resultado)
+                
+            except grpc.RpcError as e:
+                print(f"Error: {e.code()}")
+                context.set_code(grpc.StatusCode.INTERNAL)
+                context.set_details("Error en la respuesta del servidor")
+                return sumadorVector_pb2.SumaResponse()
+
+    def serve():
+        server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        sumadorVector_pb2_grpc.add_CentroCalculoServicer_to_server(CentroCalculo(), server)
+        
+        #Se escucha en el puerto 50050 por el cliente 
+        server.add_insecure_port('[::]:50050') 
+        server.start()
+        server.wait_for_termination()
+
+    if __name__ == '__main__':
+        serve()
---
+++ cliente.py
@@ -0,0 +1,21 @@
+    import grpc
+    import sumadorVector_pb2
+    import sumadorVector_pb2_grpc
+
+    def run():
+        '''Se solicita ingresar los números y después se convierten a un vector entero tomando en cuenta
+        ///las comas'''
+        input_numeros = input("Ingrese los números separados por comas (ejemplo: 1,2,3,4): ")
+        numeros = [int(numero) for numero in input_numeros.split(",")]  
+
+        '''Nos conectamos al canal para recibir y enviar mensajes con el centro de calculo y se llama
+        ///al STUB para poder usar los métodos necesarios'''
+        channel = grpc.insecure_channel('localhost:50050')
+        stub = sumadorVector_pb2_grpc.CentroCalculoStub(channel)
+
+        '''Finalmente se envia el vector en el forma del VECTORREQUEST y se espera a la respuesta'''
+        response = stub.SumarVector(sumadorVector_pb2.VectorRequest(numeros=numeros))
+        print(f"Resultado de la suma: {response.resultado}")
+
+    if __name__ == '__main__':
+        run()
