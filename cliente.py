95418f0b9d83ed2e30a23629ee53802533fe6162
na-jimenezc <141644016+na-jimenezc@users.noreply.github.com>
2025-03-08 18:47:46 -0500

    INDETACION CORRECTA

--- cliente.py
+++ cliente.py
@@ -1,8 +1,8 @@
-    import grpc
-    import sumadorVector_pb2
-    import sumadorVector_pb2_grpc
+import grpc
+import sumadorVector_pb2
+import sumadorVector_pb2_grpc
 
-    def run():
+def run():
         '''Se solicita ingresar los números y después se convierten a un vector entero tomando en cuenta
         ///las comas'''
         input_numeros = input("Ingrese los números separados por comas (ejemplo: 1,2,3,4): ")
@@ -17,5 +17,5 @@
         response = stub.SumarVector(sumadorVector_pb2.VectorRequest(numeros=numeros))
         print(f"Resultado de la suma: {response.resultado}")
 
-    if __name__ == '__main__':
+if __name__ == '__main__':
         run()
